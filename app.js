c0fcd434f97c280026273bedcaece43fee43e792
BjornBjornson <mfitzay@gmail.com>
2017-05-18 12:19:13 +0800

    included body-parser.
    added basic response capability re:post requests. trying to make JSON understood.

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const db = require('./db.js')
-
+var bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 
 const port = 3000;
 app.get('/', (request, res)=>{
-	console.log(request.url)
-	console.log(request.query)
+	console.log(request.url);
+	console.log(request.query);
+	console.log("that's another one");
+	res.send("Hello World");
+});
+app.post('/', (request, res)=>{
+	console.log(request.url);
+	console.dir(request.body);
+	console.log("that's another post");
 	res.send("Hello World");
 });
 app.listen(port, () => {
