8824ab1ac2a8d682bd2e65b8273261cee1af0905
BjornBjornson <mfitzay@gmail.com>
2017-05-18 18:00:59 +0800

    minor tweaks.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const db = require('./db.js')
 var bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({ extended: true })); //someone figure out what the extended refers to.
 
 const port = 3000;
 app.get('/', (req, res)=>{
@@ -15,6 +16,12 @@ app.post('/', (req, res)=>{
 	console.log(req.url);
 	console.log(req.body);
 	console.log("that's another post");
+	res.send("Hello World");
+});
+app.post('/login', (req, res)=>{
+	console.log(req.url);
+	console.log(req.body);
+	console.log("that's a login attempt");
 	res.send("Hello World");
 });
 app.listen(port, () => {
