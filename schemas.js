2b73b687144141dedde55a96ab99319d65c2cc18
thebluerecluse <dkennethxdx@gmail.com>
2017-05-19 16:55:05 +0800

    Update schemas.js

--- schemas.js
+++ schemas.js
@@ -6,28 +6,28 @@ var userSchema = new Schema(
         username: { type: String, required: true, unique: true },
         email: { type: String, required: true, unique: true },
         password: {type: String, required: true },
-        region: { enum: ['NA','LA','EU','CK','OCE'], required: true },
-        ageGroup: { enum: ['13-18','19-24','25-30','30+'], required: true },
-        active: { enum: ['Morning','Afternoon','Night','Nocturnal'], required: true },
-        blacklist: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+        region: { type: String, enum: ['NA','LA','EU','CK','OCE'], required: true },
+        ageGroup: { type: String, enum: ['13-18','19-24','25-30','30+'], required: true },
+        active: { type: String, enum: ['Morning','Afternoon','Night','Nocturnal'], required: true },
+        blacklist: [{ type: Schema.ObjectId, ref: 'User' }]
     }
 )
 
 var groupSchema = new Schema(
     {
         name: { type: String, required: true, unique: true },
-        game: { enum: ['LOL','DOTA','HOTS'], required: true },
-        mode: { enum: ['Ranked','Casual','Both'], required: true },
-        region: { enum: ['NA','LA','EU','CK','OCE'], required: true },
-        players: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+        game: { type: String, enum: ['LOL','DOTA','HOTS'], required: true },
+        mode: { type: String, enum: ['Ranked','Casual','Both'], required: true },
+        region: { type: String, enum: ['NA','LA','EU','CK','OCE'], required: true },
+        players: [{ type: Schema.ObjectId, ref: 'User' }],
         roles: { type: Array, required: true, 'default': [] }  
     }
 )
 
 var mailSchema = new Schema(
     {
-        from: { type: Schema.Types.ObjectId, ref: 'User' },
-        to: { type: Schema.Types.ObjectId, ref: 'User' },
+        from: { type: Schema.ObjectId, ref: 'User' },
+        to: { type: Schema.ObjectId, ref: 'User' },
         message: String,
         timestamp: Date
     }
