1b604554137e1c3c70260edc5796930921c1b3dd
BjornBjornson <mfitzay@gmail.com>
2017-05-22 10:39:10 +0800

    handing some control over to passport.
    seems like passport will manage the responses on login/logout/signup. Shouldn't be too much of an issue, since we'll still be crafting the responses for any other post data.

--- app.js
+++ app.js
@@ -17,14 +17,16 @@ app.use(passport.session());
 //Passport 
 passport.use('login', new LocalStrategy({ //how to handle login routines
     passReqToCallback : true //To pass the request to this function
+	usernameField : 'UPE',
+    passwordField : 'AUTHC',
   }, 
-  function(req, UPE, password, done) { //remember to encrypt the password at some point
-    user = dbConnect.findEntry({ 'email': UPE }, 'User');
+  function(req, UPE, AUTHC, done) { //remember to encrypt the password at some point
+    var user = dbConnect.findEntry({ 'email': UPE }, 'User');
     if (err) { return done(err); }
     if (user == "No entry found") {
         return done(null, false, { message: 'Incorrect username.' });
     }
-    if (!user.password != password) {
+    if (!user.password != AUTHC) { //add in some sort of hashing function here.
       return done(null, false, { message: 'Incorrect password.' });
     }
     return done(null, user);
@@ -33,22 +35,22 @@ passport.use('login', new LocalStrategy({ //how to handle login routines
 ));
 
 passport.use('newUser', new LocalStrategy({ //how to handle login routines
-// possibly not even neccessary to use passport for this, note: still in psuedocode.
     passReqToCallback : true //To pass the request to this function
   }, 
-	function(req, done); //oh this is sooo wrong, going to have to either split this into different parts, and roll it beforehand, or integrate the done code. Currently just doing a first waft of a plan.
-	if (dbConnect.findEntry(req.username, "User")!= "No entry found"){
+	function(req, username, email, done){ //oh this is sooo wrong, going to have to either split this into different parts, and roll it beforehand, or integrate the done code. Currently just doing a first waft of a plan.
+	if (dbConnect.findEntry(username, "User")!= "No entry found"){
 		console.log("username exists");
-		return "UName taken";
+		return done(null, false, { message: 'Username taken'};
 	}
-	else if(dbConnect.findEntry(req.email, "User")!= "No entry found"){
+	else if(dbConnect.findEntry(email, "User")!= "No entry found"){
 		console.log("Email already in use");
-		return "Email taken";
+		return done(null, false, { message: 'Email taken'};
 	}
 	else{
-		dbConnect.createEntry(req.body, "User");
+		var user = dbConnect.createEntry(req.body, "User");
+		return done(null, user);
 	});
-));
+	)});
 
 
 passport.serializeUser(function(user, done) {
@@ -60,40 +62,6 @@ passport.deserializeUser(function(id, done) {
   });
 });
 
-function validateLogin(object){ //Basic format for making sure all the fields are correctly formed.
-	console.log(object);
-	var mail = object.UPE; //User EMail field
-	var Pass = object.AUTHC; //User's Password
-	var correct = true; // Assuming it's correctly formatted until proven elsewise
-	if(Pass.length < 7){
-		correct = false;
-		return correct; 
-	}
-	else{
-		console.log(mail);
-		var locA = mail.indexOf("@"); //location of At
-		var locD = mail.lastIndexOf("."); //Location of Dot
-		if ( (locA < 1) || ( locD - locA < 2 ) ) {
-			correct = false;
-			return correct;
-		}
-	}
-	if(correct){
-		console.log(Pass);
-		console.log("Looks ok");
-		console.log("Check the database here");
-		var postThing = { //undebugged. Might have to make a string, then run JSON.parse(thingy) on it.
-			Email: mail,
-			Password: Pass
-		};
-		standInForDBInterpreter(postThing, 'User');
-		//passport.authenticate('login',(mail, Pass)); //this is wrong, but considering whether the rest of the checks are redundant, will just call on login hit.
-		return "<this is an auth token>"; //placeholder for login confirmation.
-	}
-};
-function standInForDBInterpreter(value, table){
-	console.log("putting thing in database");
-};
 app.get('/', (req, res)=>{
 	console.log(req.url);
 	console.log(req.query);
@@ -106,7 +74,7 @@ app.post('/', (req, res)=>{
 	console.log("that's another post");
 	res.send("Hello World");
 });
-app.post('/users', (req, res)=>{
+app.post('/signup', (req, res)=>{
 	console.log(req.headers)
 	console.log(req.url);
 	console.log(req.body);
@@ -118,17 +86,23 @@ app.post('/users', (req, res)=>{
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.send(tosend);
 });
-app.post('/login', (req, res)=>{
+app.post('/login', passport.authenticate('login', {
+	successRedirect: '/home';
+	failureRedirect: '/';
+}));
+/*(req, res)=>{
 	console.log(req.headers)
 	console.log(req.url);
 	console.log(req.body);
 	console.log("that's another login");
+	passport
 	res.ContentType =('text/plain');
 	res.status = 200;
 	var tosend = validateLogin(req.body);
 	res.header("Access-Control-Allow-Origin", "*"); //currently neccesary
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.send(tosend); //must institure some variety of redirect on success/failure.
+	
 });
 app.listen(port, () => {
   console.log('Server start on port ' +port);
