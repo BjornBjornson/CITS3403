50e305a63949576a562055ad2a9ec4a77fe4ca78
BjornBjornson <mfitzay@gmail.com>
2017-05-24 20:43:25 +0800

    commentary and fixing one error

--- Front-end/HTTP-send.js
+++ Front-end/HTTP-send.js
@@ -1,29 +1,28 @@
 var http = null;
-		function validate(){
+		function validate(){  // This validates the shape of a login request. Not a particularly in-depth validation, but better than nought.
 			var Pass = document.getElementById("Password").value;
 			var mail = document.getElementById("Email").value;
 			var valid = true;
-			if( Pass.length < 7 ){
+			if( Pass.length < 7 ){ //checking password meets length requirement
 				alert("Your Password must be at least 7 characters long");
 				valid = false;
 			}
-			else{
-				var locA = mail.indexOf("@");
+			else{ //at this point password has been ok'd
+				var locA = mail.indexOf("@");// this bit checks to see if the email is roughly the right shape.
 				var locD = mail.lastIndexOf(".");
-				if ( (locA < 1) || ( locD - locA < 2 ) ) {
+				if ( (locA < 1) || ( locD - locA < 2 ) ) {// making sure that the last '.' is after the '@'
 					alert("Enter a valid email address");
 				valid = false;
 				}
 			}
 			if(valid){
-				document.getElementById('myForm').submit();
+				document.getElementById('myForm').submit();//submits the login request.
 			}
 			else{
 				return valid;
 			}	
 		}
-	function validateSignup(){ //not getting called anymore.
-		//was getting a wierd glitch when I just used them from a form. If I recall correctly this does a shallow copy.
+	function validateSignup(){ //checks that the signup details are roughly the right shape.
 		var User= document.getElementById("Username").value;
 		var Pass = document.getElementById("Password").value;
 		var mail = document.getElementById("Email").value;
@@ -50,33 +49,33 @@ var http = null;
 		}
 		return valid;
 	}
-	//on page load
+	//on page load checks the user's group panel can be filled out, and drops the time at the bottom of the page.
+	//also triggers putting in the logout link.
 	function toLoad(){
 		fetchMyTeams();
 		var time = new Date();
 		document.getElementById('footTime').innerHTML = "It is now: " +time.getHours()+":"+time.getMinutes()+":"+time.getSeconds()+ "    Last updated on: 10/4/17" ;
 	}
-	function toGroupPage(group){
+	function toGroupPage(group){ //navigates to the a group's page on request.
 		var whereTo ="groupPage.html?"+group;
 		alert(whereTo);
 		window.location = whereTo;
 	}
 	//populates the mygroups window
 	function fetchMyTeams(){
-		
 		http = new XMLHttpRequest();
 		http.onreadystatechange = showTeams; //will need to debug before posting.
-		http.open("GET", "http://localhost:3000/mygroups", true); //currently a-synchronous iirc
+		http.open("GET", "http://localhost:3000/mygroups", true); //currently a-synchronous iirc, also limited to self hosted servers.
 		http.send();
-		//document.getElementById('myGroups').innerHTML = "<table><tr><td>'This could take a moment, please wait'</td></tr></table>";
+		//document.getElementById('myGroups').innerHTML = "<table><tr><td>'This could take a moment, please wait'</td></tr></table>"; // optional loading text.
 	}
-	function showTeams(){
+	function showTeams(){ // if the user is logged in, this will show their groups.
 		if (http.readyState==4) {
 			if(http.responseText=="You're not logged in"){
 				document.getElementById('myGroups').innerHTML ="<tr><td class='searchReturn'>Log in to see more!</td></tr>";
 			}
 			else{
-				document.getElementById('logoutLink').innerHTML = 'Logout';
+				document.getElementById('logoutLink').innerHTML = 'Logout'; //it also triggers the display of the logout link.
 				var myArray = JSON.parse(http.responseText);
 				console.log(myArray);
 				var myTable = "<table class='tablePrint' id='groupList'>";
